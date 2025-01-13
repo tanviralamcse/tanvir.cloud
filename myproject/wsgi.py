259b624d924491f90053ea59c58fc4270f164b4c
tanviralamcse <tanviralam.cse@gmail.com>
2025-01-13 14:41:23 +0100

    wsgi and package.json added

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -4,4 +4,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
 application = get_wsgi_application()
-app = application  # Add this line for Vercel compatibility
