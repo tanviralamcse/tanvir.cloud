986ff4631ca639022e2cba43fe9a7ffc3153e8a5
tanviralamcse <tanviralam.cse@gmail.com>
2025-01-13 15:33:08 +0100

    wsgi and package.json added

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 from .views import main
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", main, name="main"),
-    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
