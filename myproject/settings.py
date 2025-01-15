1b4c41a8f4b7b02a142db4bdec89214f09d4aab8
tanviralamcse <tanviralam.cse@gmail.com>
2025-01-15 16:03:21 +0100

    Added a third change to the vercel.json

--- myproject/settings.py
+++ myproject/settings.py
@@ -52,7 +52,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "myproject.wsgi.app"
+WSGI_APPLICATION = "myproject.wsgi.application"
 
 
 # Database
--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -3,4 +3,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
-app = get_wsgi_application()
+application = get_wsgi_application()
+
+app = application
